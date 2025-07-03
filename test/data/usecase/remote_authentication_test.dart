34fee9fdf96fd598260cbd1d851afe46f74de0dd
Mike Silva <mike.silva@pixter.com.br>
2025-07-03 20:21:36 -0300

    style: organize order imports file remote_authentication_test
    Digite os detalhes do commit (pressione Enter duas vezes para finalizar):

--- test/data/usecase/remote_authentication_test.dart
+++ test/data/usecase/remote_authentication_test.dart
@@ -1,12 +1,13 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_tdd_clean_architecture/data/usecases/usecases.dart';
-import 'package:flutter_tdd_clean_architecture/domain/usecases/authentication.dart';
-import 'package:flutter_tdd_clean_architecture/data/http/http_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 import 'remote_authentication_test.mocks.dart';
+
+import 'package:flutter_tdd_clean_architecture/domain/usecases/authentication.dart';
+
+import 'package:flutter_tdd_clean_architecture/data/usecases/usecases.dart';
+import 'package:flutter_tdd_clean_architecture/data/http/http_client.dart';
 
 @GenerateMocks([HttpClient])
 void main() {
