2ce31cfb5ca0ea1a46fd110511fde79d3bacbd9b
Mike Silva <mike.silva@pixter.com.br>
2025-07-04 18:48:24 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data
    Digite os detalhes do commit (pressione Enter duas vezes para finalizar):

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,4 +1,6 @@
-import 'package:flutter_tdd_clean_architecture/domain/entities/account_entity.dart';
+import '../../domain/entities/account_entity.dart';
+
+import '../http/http_error.dart';
 
 class RemoteAccountModel {
   final String accessToken;
@@ -6,6 +8,9 @@ class RemoteAccountModel {
   RemoteAccountModel({required this.accessToken});
 
   factory RemoteAccountModel.fromJson(Map json) {
+    if (!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
     return RemoteAccountModel(accessToken: json['accessToken']);
   }
 
--- lib/domain/entities/account_entity.dart
+++ lib/domain/entities/account_entity.dart
@@ -2,8 +2,4 @@ class AccountEntity {
   final String token;
 
   AccountEntity({required this.token});
-
-  factory AccountEntity.fromJson(Map json) {
-    return AccountEntity(token: json['accessToken']);
-  }
 }
